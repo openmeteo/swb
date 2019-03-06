adbc4ea1b2292f3d5460a67bd3aca145abd3cfa7
Antonis Christofides <antonis@antonischristofides.com>
2019-03-06 22:02:48 +0200

    Implement model (fixes #1)

--- swb/swb.py
+++ swb/swb.py
@@ -1,20 +1,109 @@
-import pandas as pd
+import numpy as np
 
 
-def calculate_soil_water(
-    *,
-    theta_s,
-    theta_fc,
-    theta_wp,
-    rd,
-    zr_factor,
-    kc,
-    p,
-    draintime,
-    effective_precipitation,
-    crop_evapotranspiration,
-    net_irrigation,
-    theta_init,
-    mif
-):
-    pd  # I use this instead of "pass" to avoid a warning
+def calculate_soil_water(**kwargs):
+    model = SoilWaterBalance(**kwargs)
+    model.calculate_timeseries()
+    return {"raw": model.raw, "taw": model.taw, "timeseries": kwargs["timeseries"]}
+
+
+class SoilWaterBalance(object):
+    # Symbols we use:
+    # theta_s - Water content at saturation
+    # theta_fc - Water content at field capacity
+    # theta_wp - Water content at wilting point
+    # theta_init - Initial water content
+    # zr - Root zone depth
+    # zr_factor - Unit change factor for reducing zr to the same unit as the time series
+    # kc - Crop coefficient
+    # p - Soil water depletion fraction for no stress
+    # draintime - Days the soil needs to go from theta_s to theta_fc
+    # raw - Readily available water
+    # taw - Total available water
+    # effective_precipitation - Effective precipitation
+    # evapotranspiration - Reference evapotranspiration
+    # crop_evapotranspiration - The reference evaporation multiplied by kc
+    # net_irrigation - The total irrigation multiplied by the irrigation efficiency
+    # ro - Surface runoff
+    # cr - Capillary rise
+    # dp - Deep percolation
+    # dr - Root zone depletion
+    # mif - Malamos irrigation factor
+
+    def __init__(self, **kwargs):
+        self.theta_s = kwargs["theta_s"]
+        self.theta_fc = kwargs["theta_fc"]
+        self.theta_wp = kwargs["theta_wp"]
+        self.zr = kwargs["zr"]
+        self.zr_factor = kwargs["zr_factor"]
+        self.p = kwargs["p"]
+        self.draintime = kwargs["draintime"]
+        self.timeseries = kwargs["timeseries"]
+        self.theta_init = kwargs["theta_init"]
+        self.mif = kwargs["mif"]
+
+        self.taw = (self.theta_fc - self.theta_wp) * self.zr * self.zr_factor
+        self.raw = self.p * self.taw
+        self.dp_when_above_field_capacity = (
+            (self.theta_s - self.theta_fc) * self.zr * self.zr_factor / self.draintime
+        )
+
+    def calculate_timeseries(self):
+        # Add columns to self.timeseries
+        self.timeseries["dr"] = np.nan
+        self.timeseries["theta"] = np.nan
+        self.timeseries["ks"] = np.nan
+        auto_apply_irrigation = "net_irrigation" not in self.timeseries
+        if auto_apply_irrigation:
+            self.timeseries["net_irrigation"] = 0.0
+
+        # Loop and perform the calculation
+        theta_prev = self.theta_init
+        dr_prev = self.dr_from_theta(theta_prev)
+        for date in self.timeseries.index:
+            row = self.timeseries.loc[date]
+            dr = self.dr(dr_prev, theta_prev, row)
+            if auto_apply_irrigation and dr > self.raw:
+                self.timeseries.at[date, "net_irrigation"] = dr * self.mif
+                dr *= 1 - self.mif
+            theta = self.theta_from_dr(dr)
+            self.timeseries.at[date, "dr"] = dr
+            self.timeseries.at[date, "theta"] = theta
+            self.timeseries.at[date, "ks"] = self.ks(dr)
+            theta_prev = theta
+            dr_prev = dr
+
+    def dr_from_theta(self, theta):
+        return (self.theta_fc - theta) * self.zr * self.zr_factor
+
+    def theta_from_dr(self, dr):
+        return self.theta_fc - dr / (self.zr * self.zr_factor)
+
+    def ks(self, dr):
+        result = (self.taw - dr) / ((1 - self.p) * self.taw)
+        return min(result, 1)
+
+    def ro(self, effective_precipitation, theta_prev):
+        result = (
+            effective_precipitation
+            + (theta_prev - self.theta_s) * self.zr * self.zr_factor
+        )
+        return max(result, 0)
+
+    def dp(self, theta):
+        if self.theta_fc >= theta:
+            return 0
+        else:
+            return self.dp_when_above_field_capacity
+
+    def dr(self, dr_prev, theta_prev, row):
+        # "row" is a single row from self.timeseries
+        return (
+            dr_prev
+            - (
+                row["effective_precipitation"]
+                - self.ro(row["effective_precipitation"], theta_prev)
+            )
+            - row["net_irrigation"]
+            + row["crop_evapotranspiration"]
+        )
---
+++ tests/test_swb.py
@@ -0,0 +1,180 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from swb import SoilWaterBalance, calculate_soil_water
+
+
+class SimpleMethodsTestCase(TestCase):
+    def setUp(self):
+        self.swb = SoilWaterBalance(
+            theta_s=0.425,
+            theta_fc=0.287,
+            theta_wp=0.14,
+            zr=0.5,
+            zr_factor=1000,
+            p=0.5,
+            draintime=16.2,
+            timeseries=None,
+            theta_init=None,
+            mif=None,
+        )
+
+    def test_taw(self):
+        self.assertAlmostEqual(self.swb.taw, 73.5)
+
+    def test_raw(self):
+        self.assertAlmostEqual(self.swb.raw, 36.75)
+
+    def test_dr_from_theta(self):
+        self.assertAlmostEqual(self.swb.dr_from_theta(0.277326), 4.837, places=3)
+
+    def test_theta_from_dr(self):
+        self.assertAlmostEqual(self.swb.theta_from_dr(4.837), 0.277, places=3)
+
+    def test_ks(self):
+        self.assertAlmostEqual(self.swb.ks(37.746), 0.973, places=3)
+
+    def test_ks_when_above_threshold(self):
+        self.assertAlmostEqual(self.swb.ks(30), 1)
+
+    def test_dp(self):
+        self.assertAlmostEqual(self.swb.dp(0.311), 4.259, places=3)
+
+    def test_dp_zero(self):
+        self.assertEqual(self.swb.dp(0.186), 0)
+
+    def test_ro_at_saturation(self):
+        # When we are already saturated (0.425), all precipitation (15 mm) runs off
+        self.assertAlmostEqual(self.swb.ro(15, 0.425), 15)
+
+    def test_ro_zero(self):
+        # When the soil is practically dry and 2 mm falls, nothing runs off
+        self.assertEqual(self.swb.ro(2, 0.14), 0)
+
+
+class ModelRunWithNetIrrigationTestCase(TestCase):
+    def setUp(self):
+        data = {
+            "effective_precipitation": [0, 4.8],
+            "net_irrigation": [0, 0],
+            "crop_evapotranspiration": [3.871, 3.990],
+        }
+        self.df = pd.DataFrame(data, index=pd.date_range("2019-03-07", periods=2))
+        calculate_soil_water(
+            theta_s=0.425,
+            theta_fc=0.287,
+            theta_wp=0.14,
+            zr=0.5,
+            zr_factor=1000,
+            p=0.5,
+            draintime=16.2,
+            timeseries=self.df,
+            theta_init=0.252784,
+            mif=None,
+        )
+
+    def test_dr1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["dr"], 20.979, places=3)
+
+    def test_dr2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["dr"], 20.169, places=3)
+
+    def test_theta1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["theta"], 0.245, places=3)
+
+    def test_theta2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["theta"], 0.247, places=3)
+
+    def test_ks1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["ks"], 1)
+
+    def test_ks2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["ks"], 1)
+
+
+class ModelRunWithoutNetIrrigationTestCase(TestCase):
+    def setUp(self):
+        data = {
+            "effective_precipitation": [0, 0, 0],
+            "crop_evapotranspiration": [3.647, 3.822, 3.885],
+        }
+        self.df = pd.DataFrame(data, index=pd.date_range("2019-03-07", periods=3))
+        calculate_soil_water(
+            theta_s=0.425,
+            theta_fc=0.287,
+            theta_wp=0.14,
+            zr=0.5,
+            zr_factor=1000,
+            p=0.5,
+            draintime=16.2,
+            timeseries=self.df,
+            theta_init=0.223646,
+            mif=1.0,
+        )
+
+    def test_dr1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["dr"], 35.324, places=3)
+
+    def test_dr2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["dr"], 0, places=3)
+
+    def test_dr3(self):
+        self.assertAlmostEqual(self.df.iloc[2]["dr"], 3.885, places=3)
+
+    def test_theta1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["theta"], 0.216, places=3)
+
+    def test_theta2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["theta"], 0.287, places=3)
+
+    def test_theta3(self):
+        self.assertAlmostEqual(self.df.iloc[2]["theta"], 0.279, places=3)
+
+    def test_net_irrigation1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["net_irrigation"], 0)
+
+    def test_net_irrigation2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["net_irrigation"], 39.146, places=3)
+
+    def test_net_irrigation3(self):
+        self.assertAlmostEqual(self.df.iloc[2]["net_irrigation"], 0)
+
+
+class MifTestCase(TestCase):
+    def setUp(self):
+        data = {
+            "effective_precipitation": [0, 0, 0],
+            "crop_evapotranspiration": [3.647, 3.822, 3.885],
+        }
+        self.df = pd.DataFrame(data, index=pd.date_range("2019-03-07", periods=3))
+        calculate_soil_water(
+            theta_s=0.425,
+            theta_fc=0.287,
+            theta_wp=0.14,
+            zr=0.5,
+            zr_factor=1000,
+            p=0.5,
+            draintime=16.2,
+            timeseries=self.df,
+            theta_init=0.223646,
+            mif=0.5,
+        )
+
+    def test_dr1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["dr"], 35.324, places=3)
+
+    def test_dr2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["dr"], 19.573, places=3)
+
+    def test_dr3(self):
+        self.assertAlmostEqual(self.df.iloc[2]["dr"], 23.458, places=3)
+
+    def test_net_irrigation1(self):
+        self.assertAlmostEqual(self.df.iloc[0]["net_irrigation"], 0)
+
+    def test_net_irrigation2(self):
+        self.assertAlmostEqual(self.df.iloc[1]["net_irrigation"], 19.573, places=3)
+
+    def test_net_irrigation3(self):
+        self.assertAlmostEqual(self.df.iloc[2]["net_irrigation"], 0)
